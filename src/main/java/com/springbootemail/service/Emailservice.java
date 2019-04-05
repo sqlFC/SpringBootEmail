fe1872364900e3191e15839a5319bcbc0d3ea97f
sqlFC <1661009855@qq.com>
2019-04-05 13:01:11 +0800

    2019-4-5

--- src/main/java/com/springbootemail/service/Emailservice.java
+++ src/main/java/com/springbootemail/service/Emailservice.java
@@ -2,9 +2,16 @@ package com.springbootemail.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.MailMessage;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 /**
  * @author zhu
@@ -21,9 +28,9 @@ public class Emailservice {
     public JavaMailSender javaMailSender;
 
     /**
-     * 发送邮件
+     * 发送文本邮件
      *
-     * @param toUser  发送给谁
+     * @param toUser  发送地址
      * @param subject 邮件主题
      * @param content 邮件内容
      */
@@ -34,5 +41,46 @@ public class Emailservice {
         simpleMailMessage.setText(content);
         simpleMailMessage.setFrom(fromUserName);
         javaMailSender.send(simpleMailMessage);
+    }
+
+    /**
+     * 发送html邮件
+     *
+     * @param toUser  发送地址
+     * @param subject 邮件主题
+     * @param content 邮件内容
+     */
+    public void sendHtmlmail(String toUser, String subject, String content) throws MessagingException {
+        MimeMessage mailMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mailMessage);
+        mimeMessageHelper.setTo(toUser);
+        mimeMessageHelper.setSubject(subject);
+        mimeMessageHelper.setText(content,true);
+        mimeMessageHelper.setFrom(fromUserName);
+        javaMailSender.send(mailMessage);
+    }
+
+    /**
+     * 发送带附件邮件
+     * @param toUser 发送地址
+     * @param subject 邮件主题
+     * @param content 邮件内容
+     * @param filePath 附件地址
+     * @throws MessagingException
+     */
+    public void sendInlinResourceMail(String toUser,
+                                      String subject,
+                                      String content,
+                                      String filePath) throws MessagingException {
+        MimeMessage mailMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mailMessage,true);
+        mimeMessageHelper.setTo(toUser);
+        mimeMessageHelper.setSubject(subject);
+        mimeMessageHelper.setText(content,true);
+        mimeMessageHelper.setFrom(fromUserName);
+        FileSystemResource file = new FileSystemResource(new File(filePath));
+        String fileName = file.getFilename();
+        mimeMessageHelper.addAttachment(fileName,file);
+        javaMailSender.send(mailMessage);
     }
 }
